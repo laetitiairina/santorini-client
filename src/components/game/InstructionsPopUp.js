627b12e1b33c35e697a944999e91224a9c1a60e4
anholg <43529669+anholg@users.noreply.github.com>
2019-05-23 06:09:34 +0200

    Update InstructionsPopUp.js

--- src/components/game/InstructionsPopUp.js
+++ src/components/game/InstructionsPopUp.js
@@ -51,7 +51,7 @@ const InstructionsPopUp = ({appears, showInstructions}) => {
             <Container>
               <Header> Instructions </Header>
               <Contents>
-                <p>Player take turns, staring with the Start Player, who first placed their Workers. On your turn, select one of your Workers. You must move and then build with the selected Worker.</p>
+                <p><b>PLAYERS</b> take turns, starting with the Start Player, who first placed their Workers. On your turn, select one of your Workers. You must move and then build with the selected Worker.</p>
                 <p><b>MOVE</b> your selected Worker into one of the (up to) eight neighboring spaces. A Worker may move up a maximum of one level higher, move down any number of levels lower, or move along the same level. The space your Worker moves into must be unoccupied (not containing a Worker or Dome).</p>
                 <p><b>BUILD</b> a Block or Dome on an unoccupied space neighboring the moved Worker. You can build onto a level of any height. A Dome can only be build on top of three blocks.</p>
                 <p>You <b>WIN</b> if one of your Workers moves up on top of 3 Blocks during your turn.</p>
